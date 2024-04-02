bfa6da8e821e1c43adb08a61bc2901262e1e5ee4
unknown <aman.rawal.cs25@iilmcet.ac.in>
2024-04-02 19:45:16 +0530

    h

---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail="rawal@gmail.com"
+var accountPassword = "12345"
+accountCity= "jaipur"
+accountEmail="aman@gamil.com"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity]);
