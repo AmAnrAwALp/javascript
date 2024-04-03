44a05504e501e89e8a2332c36c19c6e99e8d0b0b
AmAnrAwALp <136057432+AmAnrAwALp@users.noreply.github.com>
2024-04-04 00:37:18 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,9 @@
+let name= "aman"//string
+let age=21//number
+let isLoggedIn= false//boolean 
+// if number if >>2^53 bigint is used 
+// null= standalone value it is an object
+// undefined 
+//symblos
+//objects
+console.log(typeof null);
---
+++ datatypes1.js
@@ -0,0 +1,14 @@
+let score = '12'
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+console.log( typeof valueInNumber);
+console.log( valueInNumber);//"33"==> 33 but "33abc"==>NAN
+let isLoggedIn=2
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);//1== true 0== false , " "== false, "aman"==true
+let someNumber = 23089
+let stringNumber=String(someNumber)
+console.log(typeof(stringNumber));
